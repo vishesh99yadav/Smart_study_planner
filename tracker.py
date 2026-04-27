ddb55c4c25bc78504da50abb4e7023fc29746ab2
KrishnaKumar-0123 <mp8srg9n5s@privaterelay.appleid.com>
2026-04-27 16:11:04 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,3 +1,9 @@
 def log(self):
   print("\n    LOG STUDY     ")
   name = input("Subject: ")
+
+  try:
+    hours=float(input("Hour(example: 2 or 2.5): "))
+  except:
+    print("invalid input! Please enter number like 2 or 2.5\n")
+    return
