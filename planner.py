b96b0ea10f10ca8a6680538bb048b0af828f4ec2
ankitsingh0311 <ankitsingh341229@gmail.com>
2026-04-27 15:56:05 +0530

    correct planner.py

--- planner.py
+++ planner.py
@@ -1,11 +1,11 @@
 def plan(self):
-        print("\n========== 🤖 AI STUDY MENTOR (REALISTIC + BALANCED) ==========")
+        print("\n==========  AI STUDY MENTOR (REALISTIC + BALANCED) ==========")
 
         if not self.subjects:
-            print("⚠️ No subjects found!\n")
+            print(" No subjects found!\n")
             return
 
         total_subjects = len(self.subjects)
 
-        print(f"\n🧠 I can see you’re handling {total_subjects} subjects.")
-        print("👉 Don’t worry — we’ll plan this smartly without overloading you.\n")
+        print(f"\n I can see you’re handling {total_subjects} subjects.")
+        print(" Don’t worry — we’ll plan this smartly without overloading you.\n")
