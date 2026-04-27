c470df8637bfbd813e0be6c5072b373cbacbeddb
vishesh99yadav <vishesh99yadav@gmail.com>
2026-04-27 16:19:36 +0530

    Implement student, subject, and study planner classes
    Added classes for student, subject, and study planner with initial attributes and methods.

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
 import json
+from datetime import datetime
+import time
+import sy
+
+class student:
+  def __init__(self,name="",course="",university=""):
+    selt.name = name
+    self.course = course
+    self.university = university
+
+  def to_dict(self):
+    return self.__dict__
+
+class subject:
+  def __init__(self,name,difficulty,exam_date,units,weak=False,
+hour_done=0):
+    self.name = name
+    self.difficulty = difficulty
+    self.exam_date = exam_date
+    self.units = units
+    self.weak = weak
+    self.hours_done = hours_done
+
+ def to_dict(self):
+   return self.__dict__
+
+class studyplanner:
+  def __init__(self):
+    self.student = students()
+    self.subjects = []
+    self.history = []
+    self.users = {}
+    self.file = None
+    self.login_system()
+
+  def load_data(self)
+    
+
+
