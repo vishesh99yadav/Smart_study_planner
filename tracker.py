fb0a38a9342753d22610648f350a26b678115c2f
KrishnaKumar-0123 <mp8srg9n5s@privaterelay.appleid.com>
2026-04-27 16:11:40 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -3,7 +3,7 @@ def log(self):
   name = input("Subject: ")
 
   try:
-    hours=float(input("Hour(example: 2 or 2.5): "))
+    hours=float(input("Hours(example: 2 or 2.5): "))
   except:
     print("invalid input! Please enter number like 2 or 2.5\n")
     return
