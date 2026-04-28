e6ea5e5d6a1c9da171ceb706c7920885d793f47b
KrishnaKumar-0123 <mp8srg9n5s@privaterelay.appleid.com>
2026-04-28 09:47:13 +0530

    Improve error handling and history view in tracker.py
    Fix indentation and add error handling for subject not found.

--- tracker.py
+++ tracker.py
@@ -7,3 +7,7 @@ def log(self):
   except:
     print("invalid input! Please enter number like 2 or 2.5\n")
     return
+  print("subject not found\n")
+  def history_view(self):
+    print("\n    HISTORY         ")
+    for hin self.history:
