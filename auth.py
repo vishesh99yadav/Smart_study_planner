fc1f84d2c2714e0298b95abfbfa841164a41e8a0
sujalsingh0skg-creator <sujalsingh0skg@gmail.com>
2026-04-29 23:49:49 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,9 +1,9 @@
 def load_users(self):
   try:
-    with open("user.json","r", encoding="utf-8")as f:
+    with open("users.json","r", encoding="utf-8")as f:
       return json.load(f)
   except:
     return{}
 def save_users(self):
-  with open("user.json",encoding="utf-8")as f:
+  with open("users.json","w",encoding="utf-8")as f:
     json.dump(self.users,f, indent=4)
