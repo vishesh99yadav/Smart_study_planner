f9f5c8257527b5a61ba98265de903e45f5e72cbe
sujalsingh0skg-creator <sujalsingh0skg@gmail.com>
2026-04-30 10:05:12 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -6,4 +6,14 @@ def load_users(self):
     return{}
 def save_users(self):
   with open("users.json","w",encoding="utf-8")as f:
-    json.dump(self.users,f, indent=4)
+    json.dump(self.users,f,indent=4)
+def login_system(self):
+  self.users= self.load_users
+
+  while True
+  printf("\n" + "="60)
+  print("USERS AUTHENTICATION SYSTEM")
+  print("="*60)
+  print("1. Register New User")
+  print(2. Login Existi user")
+  print("3. Exit");
